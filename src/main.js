248d5209df7471cf08a042bfe7be89f542fb8d18
d_mikhailets <d_mikhailets@wargaming.net>
2023-05-03 11:45:20 +0300

    change loading title

--- src/main.js
+++ src/main.js
@@ -28,7 +28,6 @@ bot.command('new', async (ctx) => {
 bot.on(message('voice'), async (ctx) => {
     ctx.session ??= INITIAL_SESSION
     try {
-        await ctx.reply(code('Loading...'))
         const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
         const userId = String(ctx.message.from.id)
         const oggPath = await ogg.create(link.href, userId)
@@ -53,7 +52,6 @@ bot.on(message('voice'), async (ctx) => {
 bot.on(message('text'), async (ctx) => {
     ctx.session ??= INITIAL_SESSION
     try {
-        await ctx.reply(code('Loading...'))
         ctx.session.messages.push({
             role: openAI.roles.USER,
             content: ctx.message.text
